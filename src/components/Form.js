822ae25b19b6890059a153ee045125b862b44286
Suelem Macedo <suelemmacedo_@hotmail.com>
2022-09-26 19:44:55 -0300

    Requisito 2 ajustes de lint

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Form extends React.Component {
   render() {
@@ -10,7 +11,7 @@ class Form extends React.Component {
       cardImage,
       cardRare,
       cardTrunfo,
-      hasTrunfo,
+      /*  hasTrunfo, */
       isSaveButtonDisabled,
       onInputChange,
       onSaveButtonClick } = this.props;
@@ -98,5 +99,21 @@ class Form extends React.Component {
     );
   }
 }
+
+Form.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.string.isRequired,
+  /* hasTrunfo: PropTypes.bool.isRequired, */
+  isSaveButtonDisabled: PropTypes.func.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  onSaveButtonClick: PropTypes.func.isRequired,
+
+};
 
 export default Form;
